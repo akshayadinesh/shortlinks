1f09ff13c516837f2e7844e192cfd553ee043f94
Akshaya Dinesh <akshayadinesh19@gmail.com>
2020-04-11 22:53:26 -0400

    multiple tabs

--- background.js
+++ background.js
@@ -22,12 +22,28 @@ chrome.omnibox.onInputEntered.addListener(
     alert('You just typed "' + text + '"');
   });
 
+
 chrome.tabs.onUpdated.addListener(function
   (tabId, changeInfo, tab) {
+var sample_alias = 'http://mm/'
+var sample_tabs = ['https://docs.google.com/spreadsheets/d/1njOuKFobhQC4wx9HRTFnE15JoWWatu6BjXdGj3pZh4I/edit#gid=1302233113',
+'https://docs.google.com/spreadsheets/d/1hDLL8XsO4-dXWJu1PnZKdMTmIDHWp6EVZJoqVwkgFTc/edit',
+'https://docs.google.com/forms/d/1iJvM0GC2O2Cj6FoFdVn7GP6xgopghZeZpjdEv-pzQ04/edit',
+'https://docs.google.com/forms/u/1/d/1XcZRkRsSSKftT8r_m3C4CncteA7J7OtqVZiUpNWBr-c/edit?usp=drive_web',
+'gmail.com']
+
     // read changeInfo data and do something with it (like read the url)
     if (changeInfo.url) {
-      if (changeInfo.url == 'http://to/') {
-        chrome.tabs.update(tabId, { url: 'https://google.com' });
+      // alert(changeInfo.url)
+      if (changeInfo.url == sample_alias) {
+        for(var i = 0; i < sample_tabs.length; i++) {
+          if (sample_tabs[i].indexOf('://') < 0) {
+            // Add a default protocol
+            sample_tabs[i] = "http://" + redirect;
+          }
+          chrome.tabs.create({url: sample_tabs[i]})
+        }
+        chrome.tabs.remove(tabId)
       }
     }
   }
